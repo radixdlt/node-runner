e4c31b40584d399d484c108013150118536c2975
shambupujar <shambu@radixdlt.com>
2021-06-20 20:49:13 +0100

    fix regex check for trustednode

--- node-runner-cli/utils/utils.py
+++ node-runner-cli/utils/utils.py
@@ -135,7 +135,7 @@ class Helpers:
     @staticmethod
     def parse_trustednode(trustednode):
         import re
-        if not bool(re.match(r"radix//.*@\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", trustednode)):
+        if not bool(re.match(r"radix://(.*)@\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", trustednode)):
             print(f"Trusted node {trustednode} does match pattern radix://public_key@ip. Please enter right value")
             sys.exit()
         return trustednode.rsplit('@', 1)[-1]
