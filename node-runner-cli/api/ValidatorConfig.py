001f58acde3692decadf00e3c7249a83e1f1db2e
Theo Christodoulou <thechristodoulou@gmail.com>
2022-04-05 21:28:59 +0200

    final fixes

--- node-runner-cli/api/ValidatorConfig.py
+++ node-runner-cli/api/ValidatorConfig.py
@@ -125,8 +125,7 @@ class ValidatorConfig:
         return actions
     
     @staticmethod
-    def cancel_vote(actions: List, validator_info: EntityResponse, health, engine_configuration):
-        #latest_fork_name = print_vote_and_fork_info(health, engine_configuration)
+    def cancel_vote(actions: List, validator_info: EntityResponse):
         response = input(f"{bcolors.BOLD}\nDo you want to cancel your vote [y/n]? " +
                          f"Presssing Enter does not cancel your vote: {bcolors.ENDC}").strip()
         if response.lower().strip() == 'y':
--- node-runner-cli/utils/utils.py
+++ node-runner-cli/utils/utils.py
@@ -242,4 +242,5 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
     
