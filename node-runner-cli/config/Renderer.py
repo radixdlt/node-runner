65deb134c1bdc7b5bc397735bbc7fa52a1b9d5f6
shambupujar <shambu@radixdlt.com>
2022-05-18 10:10:37 +0100

    Update pip file and pyinstaller spec file

--- node-runner-cli/config/Renderer.py
+++ node-runner-cli/config/Renderer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
@@ -6,9 +7,11 @@ from jinja2 import Environment, FileSystemLoader
 
 class Renderer:
     def load_file_based_template(self, template_file_name: str, template_path="templates"):
-        self.env = Environment(loader=FileSystemLoader(f"{os.getcwd()}/{template_path}"), trim_blocks=True,
+        bundle_dir = getattr(sys, '_MEIPASS', os.getcwd())
+        path_to_template = os.path.abspath(os.path.join(bundle_dir, template_path))
+        self.env = Environment(loader=FileSystemLoader(path_to_template), trim_blocks=True,
                                lstrip_blocks=True)
-        self.template = self.env.get_template(f"{template_file_name}")
+        self.template = self.env.get_template(template_file_name)
         return self
 
     def render(self, config):
