68870027ba1eaefc0637cb779738f209f14fcd81
shambupujar <shambu@radixdlt.com>
2022-06-06 11:48:16 +0100

    Add java options as template var

--- node-runner-cli/config/DockerConfig.py
+++ node-runner-cli/config/DockerConfig.py
@@ -26,6 +26,12 @@ class CoreDockerSettings(BaseConfig):
     enable_transaction: str = "false"
     existing_docker_compose: str = f"{Path.home()}/radix-fullnode-compose.yml"
     trusted_node: str = None
+    java_opts: str = "--enable-preview -server -Xms8g -Xmx8g  " \
+                     "-XX:MaxDirectMemorySize=2048m " \
+                     "-XX:+HeapDumpOnOutOfMemoryError -XX:+UseCompressedOops " \
+                     "-Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts " \
+                     "-Djavax.net.ssl.trustStoreType=jks -Djava.security.egd=file:/dev/urandom " \
+                     "-DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 
     def __iter__(self):
         class_variables = {key: value
