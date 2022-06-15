af7409fc8b03d7cf0c8f3ec50ea19abc1437d9f3
shambupujar <shambu@radixdlt.com>
2022-06-15 11:49:25 +0100

    fix typo

--- node-runner-cli/utils/utils.py
+++ node-runner-cli/utils/utils.py
@@ -261,8 +261,8 @@ class Helpers:
 
     @staticmethod
     def get_default_node_config_dir():
-        return f"{Path.home()}/node-config" \
- \
+        return f"{Path.home()}/node-config"
+
     @staticmethod
     def get_default_monitoring_config_dir():
         return f"{Path.home()}/monitoring"
