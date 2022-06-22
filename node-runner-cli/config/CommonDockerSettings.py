2fff113a102cedef15d3745fdeea16bb55cd8214
shambupujar <shambu@radixdlt.com>
2022-06-23 00:52:16 +0100

    use hardcoded nginx tag for testing

--- node-runner-cli/config/CommonDockerSettings.py
+++ node-runner-cli/config/CommonDockerSettings.py
@@ -61,7 +61,7 @@ class CommonDockerSettings(BaseConfig):
 
     def ask_nginx_release(self):
         latest_nginx_release = github.latest_release("radixdlt/radixdlt-nginx")
-        self.nginx_settings.release = latest_nginx_release
+        self.nginx_settings.release = "DO-574-setup-and-config-command-updates-a9029d5"
         if "DETAILED" in SetupMode.instance().mode:
             self.nginx_settings.release = Prompts.get_nginx_release(latest_nginx_release)
 
