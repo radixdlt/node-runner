254a6cdda8e8e42d16b8866c572d94bc7a9ae07e
shambupujar <shambu@radixdlt.com>
2022-06-23 00:15:26 +0100

    Add print error

--- node-runner-cli/utils/utils.py
+++ node-runner-cli/utils/utils.py
@@ -31,6 +31,7 @@ def run_shell_command(cmd, env=None, shell=False, fail_on_error=True, quite=Fals
     else:
         result = subprocess.run(cmd, shell=shell)
     if fail_on_error and result.returncode != 0:
+        print(result.stderr)
         print("""
             Command failed. Exiting...
         """)
